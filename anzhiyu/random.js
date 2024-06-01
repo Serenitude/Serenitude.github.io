daa4a1bab0d080453a69c816c2c74ff27fb21513
Serenitude <1406292548@qq.com>
2024-06-01 21:17:38 +0500

    Site updated: 2024-06-01 21:17:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/01/hello-world/"];function toRandomPost(){
+var posts=["2024/06/01/hello-world/","2024/06/01/小女孩/","2024/06/01/小河流水/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
